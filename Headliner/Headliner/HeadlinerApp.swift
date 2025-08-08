bacf93875b4e20ea6f2eab10c3e2a780e8b3d98c
숩 <realhsb@gmail.com>
2025-08-08 20:43:51 +0900

    initial project

---
+++ Headliner/Headliner/HeadlinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlinerApp.swift
+//  Headliner
+//
+//  Created by Soop on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HeadlinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
