9254d5fba594822d5ba1f19521c1fcfa45c6680f
Rama <gudrms2538@gmail.com>
2025-08-28 01:40:16 +0900

    feat: SwiftData에서 CRUD를 위한 ModelContext를 주입하기 위해 DIContainer로부터 Context 주입

--- Headliner/Headliner/Source/Presentation/Main/PlaylistViewModel.swift
+++ Headliner/Headliner/Source/Presentation/Main/PlaylistViewModel.swift
@@ -6,25 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
+@MainActor
 class PlaylistViewModel: ObservableObject {
     var container: DIContainer
     
-    init(
-        container: DIContainer
-    ) {
+    init(container: DIContainer) {
         self.container = container
     }
 }
 
 extension PlaylistViewModel {
     /// 플레이리스트가 비어있는지 확인
-    func isEmptyPlaylist() -> Bool {
-        container.managers.playlistDataManager.isEmpty()
-    }
+//    func isEmptyPlaylist() -> Bool {
+//        // container.managers.playlistDataManager.isEmpty()
+//    }
     
     /// 플레이리스트 전체 목록 조회
-    func getPlaylist() -> [PlaylistMusic] {
-        container.managers.playlistDataManager.getPlaylists()
+//    func getPlaylist() -> [PlaylistMusic] {
+//        // container.managers.playlistDataManager.getPlaylists()
+//    }
+
+    func deleteItems(
+        at offsets: IndexSet,
+        from playlists: [PlaylistMusic]
+    ) {
+        guard let modelContext = container.modelContext else {
+            print("@Log - ModelContext 사용 불가능")
+            return
+        }
+        
+        for offset in offsets {
+            let playlistItem = playlists[offset]
+            modelContext.delete(playlistItem)
+        }
     }
 }
